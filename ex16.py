c5d1b7eba05a03fb84810abefb99ce5fce7c9d28
Chris Jin <78185795+fscj-4raxhel@users.noreply.github.com>
2024-03-22 14:16:43 -0400

    Completed exercise 16 study drill
    Confirmed that truncate is not required for a file opened in 'w' mode
    new line character joining the sequence of lines is simpler and more elegant than the verbose way

--- ex16.py
+++ ex16.py
@@ -12,7 +12,7 @@ print("Opening the file...")
 target = open(filename, 'w')
 
 print("Truncating the file. Goodbye!")
-target.truncate()
+# target.truncate()
 
 print("Now I'm going to ask you for three lines.")
 
@@ -22,12 +22,14 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write("\n".join((line1,line2,line3)))
+
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
 
 print("And finally, we clsoe it.")
 target.close()
