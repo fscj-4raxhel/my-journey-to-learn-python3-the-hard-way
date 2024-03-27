bf0e1e7c3b9735654ce6fcb417d3da2c18bb9594
Chris Jin <78185795+fscj-4raxhel@users.noreply.github.com>
2024-03-27 14:30:28 -0400

    Completed exercise 34
    Introduction to zero-based index

---
+++ ex34.py
@@ -0,0 +1,7 @@
+# This exercise is an introduction to 0 based index.
+# Although the cardinal vs ordinal number description depicts
+# the difference between 1 based and 0 based index well, for those
+# who understands the offset concept, "first" place being offset 0
+# comes more straightfoward.
+
+# No code needed
