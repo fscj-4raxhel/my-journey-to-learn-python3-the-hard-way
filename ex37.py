8ed23645d246271f03adc004d8361082e16ee997
Chris Jin <78185795+fscj-4raxhel@users.noreply.github.com>
2024-03-27 16:23:18 -0400

    Completed excercise 37
    Symble review

---
+++ ex37.py
@@ -0,0 +1,5 @@
+# This excercise is about memorizing Python keywords, built-in data types,
+# String escape sequence and old style string formats
+
+print("I remember back to early 2020, I indeed memorized all python 3.6 keywords.")
+print("I'm too old for facts now. I'm mature enough for understanding concepts.")
