6feeacade7bf2d6a360b91246550b4f020fd04ab
Chris Jin <78185795+fscj-4raxhel@users.noreply.github.com>
2024-03-27 16:20:16 -0400

    Completed exercise 36
    Introducing debugging. Nothing to say.

---
+++ ex36.py
@@ -0,0 +1,5 @@
+# This exercise is about debugging.
+# Zed recommended for printing debugging messages instead of debugger
+# because debugger for the current level of coding it over kill.
+
+print("I agree.")
